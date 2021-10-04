4ad5b0841212706260e3589586c20e16fdbee217
Lemon <beaver@dc7342.im>
2021-10-05 01:32:35 +0500

    correct README.md

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 git clone https://github.com/lemonbro627/watcher_back
 cd watcher_back
 python manage.py migrate
+python manage.py createsuperuser
 python manage.py runserver
 ```
+### After this go to [Django admin](http://127.0.0.1/admin/) and log in
 
+---
 ## Run after `git pull`
 ```
 python manage.py migrate
--- watcher/models.py
+++ watcher/models.py
@@ -19,7 +19,7 @@ class Video(models.Model):
     start_date = models.DateField(verbose_name='Дата начала показа (включая)', default=datetime.date.today(), null=False, blank=False, editable=True)
     end_date = models.DateField(verbose_name='Дата конца паказа(включая)', default=datetime.date.today(), null=False, blank=False, editable=True)
     freq = models.CharField(max_length=9, choices=VideoFreq, default='MID', verbose_name='Частота показа', null=False, blank=False)
-    video_url = models.URLField(verbose_name='Ссылка на видео', null=True, blank=False)
+    video_url = models.URLField(verbose_name='Ссылка на видео (только прямые ссылки на mp4)', null=True, blank=False)
     is_enabled = models.BooleanField(verbose_name='Показывать?', default=True, null=False, blank=False)
 
     def __str__(self):
@@ -32,7 +32,7 @@ class Photo(models.Model):
         verbose_name_plural = 'Фото'
 
     title = models.CharField(max_length=64, verbose_name='Заголовок')
-    photo = models.FileField(verbose_name='Картинка', upload_to='', null=True, blank=False)
+    photo = models.FileField(verbose_name='Картинка (height 620px, wight 420px)', upload_to='', null=True, blank=False)
     text_before = models.CharField(max_length=128, verbose_name='Текст над картинкой (128 символов максимум)', null=True, blank=True)
     text_after = models.CharField(max_length=64, verbose_name='Текст под картинкой (64 символа максимум)', null=True, blank=True)
     is_enabled = models.BooleanField(verbose_name='Показывать?', default=True, null=False, blank=False)
